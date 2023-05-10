2857eed7cd648ad80798f3901c1f1a0c80a0189f
Szeliga <gfszeliga@gmail.com>
2023-05-10 10:34:08 +0200

    Tests for simple string digit calculator

---
+++ TDDKata2/TDDKataCalc/Calc.cs
@@ -0,0 +1,24 @@
+﻿namespace TDDKataCalc
+{
+    public class Calc
+    {
+        public int Add(string digits)
+        {
+            int parsedNumber;
+            if (string.IsNullOrEmpty(digits))
+            {
+                return 0;
+            }
+            else if(digits.Length < 3 && Int32.TryParse(digits, out parsedNumber))
+            {
+                var firstNumber = parsedNumber % 10;
+                var secondNumber = parsedNumber / 10;
+                return firstNumber + secondNumber;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
